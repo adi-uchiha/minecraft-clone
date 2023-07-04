96519a2fc33cb81aa3953d59921686b12be6c445
adi-uchiha <adi.cs.night@gmail.com>
2023-07-04 19:13:28 +0530

    grass plane buffer ready

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,11 +1,17 @@
 import { usePlane } from "@react-three/cannon"
 import { groundTexture } from "../images/textures"
+import { NearestFilter, RepeatWrapping } from "three"
 
 export const Ground = () => {
     const [ref] = usePlane(()=> ({
-        rotation: [0,0,0],
+        rotation: [-Math.PI/4,0,0],
         position: [0,0,0]
     }))
+
+    groundTexture.magFilter = NearestFilter
+    groundTexture.wrapS = RepeatWrapping
+    groundTexture.wrapT = RepeatWrapping
+    groundTexture.repeat.set(100, 100)
 
     return (
         <mesh ref={ref}>
